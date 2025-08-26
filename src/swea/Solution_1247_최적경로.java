1b9aee2dd35ab455254ede0502c0cc1f62f855b4
이승환 <seaung13@gmail.com>
2025-08-27 00:12:57 +0900

    feat: SWEA 1247번 최적 경로 추가

--- src/swea/Solution_1247_최적경로.java
+++ src/swea/Solution_1247_최적경로.java
@@ -1,11 +1,35 @@
 package swea;
 
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
+/**
+ * 아이디어:
+ * DFS 순열 + 백트래킹
+ *
+ * 회사(0) -> 고객(1~N) -> 집(N+1)
+ * -> 전체 노드 수가 정해져있다
+ * -> 순열로 접근하기
+ *
+ * 이동거리가 짧은 경로를 찾아야 함
+ * -> 가지치기를 통해 min보다 totalDist가 크면 return 합니다
+ */
+
+/**
+ * 메모리: 26,808 KB
+ * 시간: 242 ms
+ * 난이도: D5
+ */
 public class Solution_1247_최적경로 {
     static int T;
     static int N;
+
+    static int[][] positions;
+    static int[] customerOrder;
+    static boolean[] visited;
+
+    static final int ROW = 0;
+    static final int COL = 1;
 
     static int min;
 
@@ -19,28 +43,28 @@ public class Solution_1247_최적경로 {
 
         for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
+            positions = new int[N + 2][2]; // 회사와 집, 고객을 모두 담을 좌표 배열
+            customerOrder = new int[N]; // 고객 순서를 담을 배열
+            visited = new boolean[N + 1]; // 고객 방문 여부
 
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int companyRow = Integer.parseInt(st.nextToken());
-            int companyCol = Integer.parseInt(st.nextToken());
+            // 회사 좌표
+            positions[0][ROW] = Integer.parseInt(st.nextToken());
+            positions[0][COL] = Integer.parseInt(st.nextToken());
 
-            int homeRow = Integer.parseInt(st.nextToken());
-            int homeCol = Integer.parseInt(st.nextToken());
+            // 집 좌표
+            positions[N + 1][ROW] = Integer.parseInt(st.nextToken());
+            positions[N + 1][COL] = Integer.parseInt(st.nextToken());
 
-            int[][] customers = new int[N][2];
-            for (int i = 0; i < N; i++) {
-                customers[i][0] = Integer.parseInt(st.nextToken());
-                customers[i][1] = Integer.parseInt(st.nextToken());
+            // 고객 좌표
+            for (int i = 1; i <= N; i++) {
+                positions[i][0] = Integer.parseInt(st.nextToken());
+                positions[i][1] = Integer.parseInt(st.nextToken());
             }
 
-            // 회사에서 출발해 고객 좌표를 모두 방문하고 집에 돌아가는 경로 중
-            // 이동거리가 가장 짧은 경로를 찾기
-            // 최단 경로 찾는 문제인데 가중값이 존재함
-            // -> 크루스칼로 풀면 되나?
-
             min = Integer.MAX_VALUE;
-            subset(0, 0);
+            permutation(0, 0);
 
             sb.append('#').append(t).append(' ').append(min).append('\n');
         }
@@ -51,16 +75,32 @@ public class Solution_1247_최적경로 {
         bw.close();
     }
 
-    private static void subset(int index, int prevNodeIndex, int totalLength) {
-        if (index == N) {
-            min = Math.min(min, totalLength);
+    private static void permutation(int depth, int totalDist) {
+        // 현재 계산된 거리가 이전에 계산했던 최소 거리보다 커지면 가지치기
+        if (totalDist > min) {
             return;
         }
 
-        for (int i = 0; i < N; i++) {
+        if (depth == N) {
+            int home = N + 1;
+            int lastCustomer = customerOrder[N - 1];
+            int lastDist = Math.abs(positions[home][ROW] - positions[lastCustomer][ROW]) + Math.abs(positions[home][COL] - positions[lastCustomer][COL]);
+            min = Math.min(min, totalDist + lastDist);
+            return;
+        }
 
-            int length = Math.abs()
-            subset(index + 1, i,totalLength + );
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                customerOrder[depth] = i;
+
+                int current = customerOrder[depth];
+                int previous = (depth == 0) ? 0 : customerOrder[depth - 1];
+
+                int nextDist = Math.abs(positions[current][ROW] - positions[previous][ROW]) + Math.abs(positions[current][COL] - positions[previous][COL]);
+                permutation(depth + 1, totalDist + nextDist);
+                visited[i] = false;
+            }
         }
     }
 }
