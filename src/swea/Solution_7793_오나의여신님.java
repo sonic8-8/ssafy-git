74a40b39b16b2b110cfb8f2e1976680b36cbaee5
이승환 <seaung13@gmail.com>
2025-08-30 12:45:36 +0900

    fix: SWEA 7793번 오! 나의 여신님 완성 추가

--- src/swea/Solution_7793_오나의여신님.java
+++ src/swea/Solution_7793_오나의여신님.java
@@ -5,11 +5,21 @@ import java.util.*;
 
 /**
  * 아이디어:
- * 1초마다 한 번씩 악마의 손아귀와 수연 위치를 동시에 업데이트 해야함
- * dfs와 bfs를 따로 쓰는게 아닌가..?
- * 하나로 두 개를 동시에 업데이트 해야함
+ * "1초 단위로" 악마의 손아귀와 수연 위치를 한 번에 업데이트 한다
+ * - 1초 단위로 업데이트 하기 위해 queue.size()와 for문을 이용합니다
+ * - 동시에 업데이트 하기위해 악마와 수연 총 2개의 queue를 사용합니다.
+ * - 시간을 편하게 관리하기 위해 수연 큐에 time까지 넣어서 관리했습니다
+ *
+ * 악마의 손아귀: 매초마다 상하좌우 인접 영역에 확장됩니다. 편하게 그냥 악마가 증식한다고 생각했음
+ * ROCK 위치는 수연과 악마의 손아귀가 이동 불가능 함
+ * SOOYEON은 GODDESS(여신) 위치로 이동해야합니다.
  */
 
+/**
+ * 메모리: 26,624 KB
+ * 시간: 92 ms
+ * 난이도: D5
+ */
 public class Solution_7793_오나의여신님 {
     static int T;
     static int N, M;
@@ -25,7 +35,6 @@ public class Solution_7793_오나의여신님 {
     static final char ROCK = 'X';
     static final char DEVIL = '*';
     static final char EMPTY = '.';
-    static final char BLOCKED = 'B';
 
     static int min;
 
@@ -38,11 +47,15 @@ public class Solution_7793_오나의여신님 {
         T = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
             map = new char[N][M];
             visited = new boolean[N][M];
 
-            int[] sooyeon = new int[2];
-            int[] devil = new int[2];
+            int[] sooyeon = new int[3]; // row, col, time
+            Queue<int[]> devilQueue = new ArrayDeque<>(); // 악마가 여러 마리 등장 할 수 있어 큐로 관리합니다.
 
             for (int row = 0; row < N; row++) {
                 String line = br.readLine();
@@ -52,30 +65,21 @@ public class Solution_7793_오나의여신님 {
                     if (map[row][col] == SOOYEON) {
                         sooyeon[0] = row;
                         sooyeon[1] = col;
+                        sooyeon[2] = 0; // time
                     }
                     if (map[row][col] == DEVIL) {
-                        devil[0] = row;
-                        devil[1] = col;
+                        devilQueue.add(new int[]{row, col});
                     }
                 }
             }
 
-            // 악마의 손아귀: 매초마다 상하좌우 인접 영역에 확장
-            // ROCK 위치는 수연과 악마의 손아귀가 이동 불가능
-            // SOOYEON은 GODDESS 위치로 이동해야함
-
-            // 시뮬레이션을 위해 맵을 복사합니다
-            char[][] copiedMap = new char[N][N];
-            for (int i = 0; i < N; i++) {
-                copiedMap[i] = map[i].clone();
-            }
-
             min = Integer.MAX_VALUE;
 
-            // 수연, 악마 위치 업데이트
-            bfs(sooyeon, devil, copiedMap);
+            // 수연, 악마 위치를 함께 탐색합니다.
+            bfs(sooyeon, devilQueue);
 
-            sb.append("#").append(t).append(" ").append(min).append("\n");
+            // 수연이 여신에게 도달하지 못한 경우 (min = -1 일 경우) GAME OVER를 출력합니다.
+            sb.append("#").append(t).append(" ").append(min == -1 ? "GAME OVER" : min).append("\n");
         }
 
         bw.write(sb.toString());
@@ -84,71 +88,82 @@ public class Solution_7793_오나의여신님 {
         bw.close();
     }
 
-    private static void bfs(int[] sooyeon, int[] devil, char[][] simulationMap) {
+    private static void bfs(int[] sooyeon, Queue<int[]> devilQueue) {
         Queue<int[]> sooyeonQueue = new ArrayDeque<>();
         sooyeonQueue.add(sooyeon);
         visited[sooyeon[0]][sooyeon[1]] = true;
 
-        Queue<int[]> devilQueue = new ArrayDeque<>();
-        devilQueue.add(devil);
+        while (!sooyeonQueue.isEmpty()) {
+            // 1. 악마 확산
+            int devilSize = devilQueue.size(); // 1초 시간 단위를 구현하기 위해 size를 이용합니다.
+            for (int i = 0; i < devilSize; i++) {
+                int[] devilCurrent = devilQueue.poll();
+                int devilRow = devilCurrent[0];
+                int devilCol = devilCurrent[1];
 
-        while (!sooyeonQueue.isEmpty() && !devilQueue.isEmpty()) {
-            int[] devilCurrent = devilQueue.poll();
-            int devilRow = devilCurrent[0];
-            int devilCol = devilCurrent[1];
+                for (int j = 0; j < 4; j++) {
+                    int nextDevilRow = devilRow + DIR_ROW[j];
+                    int nextDevilCol = devilCol + DIR_COL[j];
 
-            int[] current = sooyeonQueue.poll();
-            int row = current[0];
-            int col = current[1];
+                    if (nextDevilRow < 0 || N <= nextDevilRow || nextDevilCol < 0 || M <= nextDevilCol) {
+                        continue;
+                    }
 
-            for (int i = 0; i < 4; i++) {
-                int nextDevilRow = devilRow + DIR_ROW[i];
-                int nextDevilCol = devilCol + DIR_COL[i];
+                    if (map[nextDevilRow][nextDevilCol] == ROCK) {
+                        continue;
+                    }
 
-                if (simulationMap[nextDevilRow][nextDevilCol] == ROCK) {
-                    continue;
-                }
-
-                if (simulationMap[nextDevilRow][nextDevilCol] == EMPTY) {
-                    simulationMap[nextDevilRow][nextDevilCol] = BLOCKED;
+                    // 지도에 악마 위치를 업데이트합니다
+                    if (map[nextDevilRow][nextDevilCol] == EMPTY) {
+                        map[nextDevilRow][nextDevilCol] = DEVIL;
+                        devilQueue.add(new int[]{nextDevilRow, nextDevilCol});
+                    }
                 }
             }
 
-            for (int i = 0; i < 4; i++) {
-                int nextRow = row + DIR_ROW[i];
-                int nextCol = col + DIR_COL[i];
+            // 2. 수연 이동
+            int sooyeonSize = sooyeonQueue.size(); // 1초 시간 단위를 구현하기 위해 size를 이용합니다
+            for (int i = 0; i < sooyeonSize; i++) {
+                int[] current = sooyeonQueue.poll();
+                int row = current[0];
+                int col = current[1];
+                int time = current[2]; // 시간을 배열에 담아서 관리합니다
 
-                if (nextRow < 0 || N <= nextRow || nextCol < 0 || N <= nextCol) {
-                    continue;
+                for (int j = 0; j < 4; j++) {
+                    int nextRow = row + DIR_ROW[j];
+                    int nextCol = col + DIR_COL[j];
+
+                    if (nextRow < 0 || N <= nextRow || nextCol < 0 || M <= nextCol) {
+                        continue;
+                    }
+
+                    if (map[nextRow][nextCol] == ROCK) {
+                        continue;
+                    }
+
+                    if (map[nextRow][nextCol] == DEVIL) {
+                        continue;
+                    }
+
+                    if (visited[nextRow][nextCol]) {
+                        continue;
+                    }
+
+                    // 수연은 지도에 위치를 업데이트하지 않고 visited 배열을 이용해 관리합니다.
+                    if (map[nextRow][nextCol] == EMPTY) {
+                        sooyeonQueue.add(new int[] {nextRow, nextCol, time + 1});
+                        visited[nextRow][nextCol] = true;
+                    }
+
+                    // 여신에게 도달 성공
+                    if (map[nextRow][nextCol] == GODDESS) {
+                        min = time + 1;
+                        return;
+                    }
                 }
-
-                if (simulationMap[nextRow][nextCol] == ROCK) {
-                    continue;
-                }
-
-                if (simulationMap[nextRow][nextCol] == BLOCKED) {
-                    continue;
-                }
-
-                if (simulationMap[nextRow][nextCol] == DEVIL) {
-                    continue;
-                }
-
-                // 이동할 수 없는지 여부 어떻게 체크할 것임?
-
-                if (simulationMap[nextRow][nextCol] == EMPTY) {
-                    simulationMap[nextRow][nextCol] = SOOYEON;
-                    simulationMap[row][col] = EMPTY;
-                    sooyeonDfs(nextRow, nextCol, totalTime + 1, simulationMap);
-                }
-
-                if (simulationMap[nextRow][nextCol] == GODDESS) {
-                    min = Math.min(min, totalTime + 1);
-                    return;
-                }
-
             }
         }
-
+        // 수연의 큐가 빌때까지 도달하지 못한 경우 -1을 반환합니다
+        min = -1;
     }
 }
