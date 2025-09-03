f8456ab51b9ab30e56b39aa9b6d30e18cd9f61aa
이승환 <seaung13@gmail.com>
2025-09-03 14:39:37 +0900

    feat: DP 예제 2번 막대 색칠하기 추가

---
+++ src/etc/Example_2.java
@@ -0,0 +1,63 @@
+package etc;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 아이디어:
+ * 나열하고 규칙 찾기
+ * =================== 1개 ===================
+ * ㅍ
+ * ㄴ
+ * dp[1] = 2;
+ * <p>
+ * =================== 2개 ===================
+ * ㅍㅍ
+ * ㅍㄴ ㄴㅍ
+ * ㄴㄴ
+ * ㅃ
+ * dp[2] = 5
+ * <p>
+ * =================== 3개 ===================
+ * ㅍㅍㅍ
+ * ㅍㅍㄴ ㅍㄴㅍ ㄴㅍㅍ
+ * ㅍㄴㄴ ㄴㅍㄴ ㄴㄴㅍ
+ * ㄴㄴㄴ
+ * ㅃㅍ ㅍㅃ
+ * ㅃㄴ ㄴㅃ
+ * dp[3] = 12
+ * <p>
+ * =================== 4개 ===================
+ * ㅍㅍㅍㅍ
+ * ㅍㅍㅍㄴ ㅍㅍㄴㅍ ㅍㄴㅍㅍ ㄴㅍㅍㅍ
+ * ㅍㅍㄴㄴ ㅍㄴㅍㄴ ㅍㄴㄴㅍ ㄴㅍㅍㄴ ㄴㅍㄴㅍ ㄴㄴㅍㅍ
+ * ㅍㄴㄴㄴ ㄴㅍㄴㄴ ㄴㄴㅍㄴ ㄴㄴㄴㅍ
+ * ㄴㄴㄴㄴ
+ * ㅃㅍㅍ ㅍㅃㅍ ㅍㅍㅃ
+ * ㅃㅍㄴ ㅃㄴㅍ ㅍㅃㄴ ㅍㄴㅃ ㄴㅃㅍ ㄴㅍㅃ
+ * ㅃㄴㄴ ㄴㅃㄴ ㄴㄴㅃ
+ * ㅃㅃ
+ * dp[4] = dp[3] * 2 + dp[2] = 16 + 13 = 29
+ */
+public class Example_2 {
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        dp = new int[7];
+
+        dp[1] = 2;
+        dp[2] = 5;
+
+        for (int i = 3; i <= 6; i++) {
+            dp[i] = dp[i - 1] * 2 + dp[i - 2];
+        }
+
+        bw.write(String.valueOf(dp[6]));
+
+        br.close();
+        bw.close();
+    }
+}
