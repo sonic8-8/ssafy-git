bfffc565b3adcffd2bb733455cb4574756f8170a
이승환 <seaung13@gmail.com>
2025-09-04 13:40:13 +0900

    feat: SWEA 2115번 벌꿀채취 추가

--- src/swea/Solution_2115_벌꿀채취.java
+++ src/swea/Solution_2115_벌꿀채취.java
@@ -3,12 +3,24 @@ package swea;
 import java.io.*;
 import java.util.*;
 
+/**
+ * 아이디어:
+ * 슬라이딩 윈도우 + 부분 집합 + DP
+ */
+
+/**
+ * 메모리: 27,264 KB
+ * 시간: 97 ms
+ * 난이도: 모의 SW 역량테스트
+ */
 public class Solution_2115_벌꿀채취 {
     static int T;
     static int N, M, C;
 
-    static int[][] board;
+    static int[][] honeyBoxes;
     static int[][] dp;
+
+    static int maxProfit;
 
     static StringBuilder sb = new StringBuilder();
 
@@ -24,36 +36,69 @@ public class Solution_2115_벌꿀채취 {
             M = Integer.parseInt(st.nextToken()); // 벌통 개수
             C = Integer.parseInt(st.nextToken()); // 꿀의 최대양
 
-            // 두 명의 일꾼이 벌꿀을 채취한다
-            // 벌통의 합이 C보다 크면 벌통 개수를 줄여야한다
-
-            board = new int[N][N];
-            dp = new int[N][N];
+            honeyBoxes = new int[N][N];
+            dp = new int[N][N - M + 1];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    board[i][j] = Integer.parseInt(st.nextToken());
-                    // M개의 벌통을 여기서 먼저 계산?
-                    // M개의 벌통 더하고 C를 초과할 경우 가장 작은 숫자 빼보기?
+                    honeyBoxes[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            // DP로 푸는건가?
-            // 슬라이딩 윈도우로 계산하기
+            // 각 행마다 M개의 연속 구간 최대 이익을 계산합니다.
+            for (int row = 0; row < N; row++) {
+                for (int col = 0; col <= N - M; col++) {
+                    // 현재 구간의 배열을 추출합니다. (M개의 연속 구간)
+                    int[] subArr = new int[M];
+                    for (int i = 0; i < M; i++) {
+                        subArr[i] = honeyBoxes[row][col + i];
+                    }
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-
+                    // 부분 집합으로 현재 구간에서 얻을 수 있는 조합 중 최대값을 찾습니다.
+                    maxProfit = Integer.MIN_VALUE;
+                    subset(0, subArr, 0, 0);
+                    dp[row][col] = maxProfit;
                 }
             }
 
-            sb.append("#").append(t).append(" ").append().append("\n");
+            // 두 일꾼이 선택할 수 있는 모든 조합 중 최댓값을 찾습니다.
+            // N이 M의 2배 이상이라면 같은 행에서 두 일꾼이 모두 벌통을 선택할 수 있습니다.
+            int max = Integer.MIN_VALUE;
+            for (int r1 = 0; r1 < N; r1++) {
+                for (int c1 = 0; c1 <= N - M; c1++) {
+                    for (int r2 = r1; r2 < N; r2++) {
+                        int cStart = (r1 == r2) ? c1 + M : 0;
+                        for (int c2 = cStart; c2 <= N - M; c2++) {
+                            max = Math.max(max, dp[r1][c1] + dp[r2][c2]);
+                        }
+                    }
+                }
+            }
+
+            sb.append("#").append(t).append(" ").append(max).append("\n");
         }
 
         bw.write(sb.toString());
 
         br.close();
         bw.close();
+    }
+
+    // 최댓값을 편하게 추적하기 위해 현재 탐색중인 구간을 잘라서 매개변수로 넣어줬습니다.
+    // sum: 벌통 숫자 합, profit: 벌통 숫자 제곱끼리 합
+    private static void subset(int index, int[] arr, int sum, int profit) {
+        if (index == arr.length) {
+            maxProfit = Math.max(maxProfit, profit);
+            return;
+        }
+
+        // 현재 인덱스 포함
+        if (sum + arr[index] <= C) {
+            subset(index + 1, arr, sum + arr[index], profit + (arr[index] * arr[index]));
+        }
+
+        // 현재 인덱스 포함 X
+        subset(index + 1, arr, sum, profit);
     }
 }
